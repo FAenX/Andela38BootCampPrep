885baba1ba82bb53e28fc6ca7395e10dec5c4175
Emmanuel Davidson <kipronofb@gmail.com>
2019-02-21 04:23:37 +0300

    first commit

---
+++ andelans/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Andelan(models.Model):
+    """[summary]
+
+    Arguments:
+        models {[type]} -- [description]
+    """
+
+    LANGUAGE_CHOICES = [
+        'python',
+        'javascript',
+        'java',
+
+    ]
+    first_name = models.CharField(max_length=30)
+    second_name = models.CharField(max_length=30)
+    language = models.CharField(max_length=30, choice=LANGUAGE_CHOICES)
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name} is doing {self.language}'
---
+++ andelans/urls.py
@@ -0,0 +1,3 @@
+from django.urls import path
+
+# andelans app url patterns
