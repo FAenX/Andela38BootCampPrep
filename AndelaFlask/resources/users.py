a28b09ebfc9715189ef2156c3b8f3139e9491da5
Emmanuel Davidson <kipronofb@gmail.com>
2019-02-25 19:18:40 +0300

    added GET and POST methods

--- AndelaFlask/resources/users.py
+++ AndelaFlask/resources/users.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, abort
 from models import db, User, UserSchema
 
 user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 
 
 class UserResource(Resource):
@@ -13,8 +14,13 @@ class UserResource(Resource):
             abort(400)  # missing arguments
         if User.query.filter_by(username=username).first() is not None:
             abort(400)  # existing user
-        user = User(username=username)
+        user = User(username=username, password_hash=password)
         # user.hash_password(password)
         db.session.add(user)
         db.session.commit()
         return {'username': user.username}, 201
+
+    def get(self):
+        users = User.query.all()
+        users = users_schema.dump(users).data
+        return {'status': 'success', 'data': users}, 200
